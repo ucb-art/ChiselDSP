58c6daaf176803ded3620bbdd9cd97da35d5fdb0
Angie <shunshou@gmail.com>
2016-02-16 17:01:04 -0800

    fixed complexlut (was not compiling verilog in fixed when i tried combining bits). NOTE SEE CHANGES TO GET YOUR CODE WORKING

--- ChiselDSP/Overlay/DSPScalaHelpers.scala
+++ ChiselDSP/Overlay/DSPScalaHelpers.scala
@@ -14,10 +14,15 @@ object List2Tuple {
 // TODO: Which is better? ZeroPadLit as string or shift by "digit width" for concatenation?
 /** Returns a string with a Lit padded (with 0's) to the desired bitwidth */
 object ZeroPadLit{
+  def apply(x: BigInt,width:Int): String = {
+    val stringVal = x.toString(2)
+    if (stringVal.length > width) Error("Width cannot be smaller than length of x in radix-2 for zero padding")
+    List.fill(width - stringVal.length)("0").mkString("").concat(stringVal)
+  }
   def apply(x: Bits): String = {
     if (!x.isLit) Error("ZeroPadLit only works on literals!")
     val width = x.getWidth
     val stringVal = x.litValue().toString(2)
-    List.fill(x.getWidth - stringVal.length)("0").mkString("").concat(stringVal)
+    List.fill(width - stringVal.length)("0").mkString("").concat(stringVal)
   }
 }

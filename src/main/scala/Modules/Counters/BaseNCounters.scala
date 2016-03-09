5f913262a591a550d2565f45429534e5e86d6a58
Angie <shunshou@gmail.com>
2016-03-09 02:41:37 -0800

    changed basen counters some more...

--- src/main/scala/Modules/Counters/BaseNCounters.scala
+++ src/main/scala/Modules/Counters/BaseNCounters.scala
@@ -9,7 +9,9 @@ case class BaseNCountParams (
   maxCoprime: Int,                // Dictates maximum count value (maxCoprime-1)
   incMax: Int = 1,                // Largest increment (if 1, then use fixed increment)
   externalWrap: Boolean = false,  // Additional wrapping condition supported?
-  inDelay: Int = 0                // Pipeline delay associated with inputs
+  inDelay: Int = 0,               // Pipeline delay associated with inputs
+  clkEn: Boolean = true,          // Whether additional enable is needed for slow clock support
+  change: Boolean = true          // Whether additional enable/change (i.e. update on previous counter max) is needed
 
 )
 
@@ -29,36 +31,39 @@ class BaseNCountIO (p: BaseNCountParams) extends IOBundle {
 
 /** Additional control signals for the Base N counter */
 class BaseNCountCtrl (p: BaseNCountParams) extends IOBundle {
-  val change = DSPBool(INPUT)
+  val en = if (p.clkEn) Some(DSPBool(INPUT)) else None
+  val change = if(p.change) Some(DSPBool(INPUT)) else None
   val reset = DSPBool(INPUT)
   val wrap = if (p.externalWrap) Some(DSPBool(INPUT)) else None
+  val isMax = DSPBool(OUTPUT)
 }
 
-/** Counter that accumulates in Base N notation */
-abstract class BaseNCounter (p: BaseNCountParams) extends DSPModule (inputDelay = p.inDelay) {
+/** Counter that accumulates in Base N notation: clock ratio is used for slow/fast clock (i.e.
+  * slow clock is 2x slower than nominal clock --> clkRatio = 2
+  */
+abstract class BaseNCounter (p: BaseNCountParams, clkRatio:Int) extends DSPModule (inputDelay = p.inDelay) {
 
+  if (clkRatio < 1) Error("Clock ratio must be >= 1")
   // For reference
   val rad = p.rad
 
   override val io = new BaseNCountIO(p)
-  val iCtrl = new BaseNCountCtrl(p)
-  val oCtrl = new BaseNCountCtrl(p).flip
+  val ctrl = new BaseNCountCtrl(p)
 
   val inc = io.inc.getOrElse( BaseN(1, p.rad, p.maxCoprime-1) )
 
   val zero = BaseN(0, p.rad, p.maxCoprime-1)
   // Get mod sum value
   val nextInSeq = io.out + inc
-  val nextCount = if (p.externalWrap) Mux(iCtrl.wrap.get,zero,nextInSeq) else nextInSeq
-  val newOnClk = Mux(iCtrl.change,nextCount,io.out)
-  val count = Mux(iCtrl.reset,zero,newOnClk)
+  val nextCount = if (p.externalWrap) Mux(ctrl.wrap.get,zero,nextInSeq) else nextInSeq
+  val newOnClk = Mux(ctrl.change.getOrElse(DSPBool(true)) & ctrl.en.getOrElse(DSPBool(true)),nextCount,io.out)
+  val count = Mux(ctrl.reset,zero,newOnClk)
   // TODO: Check critical path with maskWithMaxCheck
   val (out,eqMax) = count.maskWithMaxCheck(io.primeDigits)
   io.out := RegNext(out)
 
-  oCtrl.reset := iCtrl.reset
   // Next counter's update might depend on this counter looping around from the maximum
-  oCtrl.change := eqMax.pipe(1) & iCtrl.change
+ ctrl.isMax := Pipe(RegNext(eqMax),clkRatio-1)
 
 }
 
@@ -68,22 +73,22 @@ abstract class BaseNCounter (p: BaseNCountParams) extends DSPModule (inputDelay 
 
 /** Base N Increment (+1) Counter */
 object BaseNIncCounter{
-  def apply(rad: Int, maxCoprime: Int, inputDelay: Int = 0, nameExt: String = "") : BaseNIncCounter = {
+  def apply(rad: Int, maxCoprime: Int, clkRatio:Int, inputDelay: Int = 0, nameExt: String = "") : BaseNIncCounter = {
     val p = BaseNCountParams(rad = rad, maxCoprime = maxCoprime, inDelay = inputDelay)
-    DSPModule(new BaseNIncCounter(p), nameExt)
+    DSPModule(new BaseNIncCounter(p,clkRatio), nameExt)
   }
 }
-class BaseNIncCounter(p: BaseNCountParams) extends BaseNCounter(p)
+class BaseNIncCounter(p: BaseNCountParams, clkRatio:Int) extends BaseNCounter(p,clkRatio)
 
 /** Base N Accumulator (runtime-dependent increments) with External Wrap */
 object BaseNAccWithWrap{
-  def apply(rad: Int, maxCoprime: Int, inputDelay: Int = 0, nameExt: String = "") : BaseNAccWithWrap = {
+  def apply(rad: Int, maxCoprime: Int, clkRatio: Int, inputDelay: Int = 0, nameExt: String = "") : BaseNAccWithWrap = {
     val p = BaseNCountParams(rad = rad, maxCoprime = maxCoprime, inDelay = inputDelay,
-                             incMax = maxCoprime-1, externalWrap = true)
-    DSPModule(new BaseNAccWithWrap(p), nameExt)
+                             incMax = maxCoprime-1, externalWrap = true, change = false)
+    DSPModule(new BaseNAccWithWrap(p,clkRatio), nameExt)
   }
 }
-class BaseNAccWithWrap(p: BaseNCountParams) extends BaseNCounter(p)
+class BaseNAccWithWrap(p: BaseNCountParams, clkRatio:Int) extends BaseNCounter(p,clkRatio)
 
 /*
   DEFAULTS
@@ -91,5 +96,7 @@ class BaseNAccWithWrap(p: BaseNCountParams) extends BaseNCounter(p)
   maxCoprime: Int,                // Dictates maximum count value (maxCoprime-1)
   incMax: Int = 1,                // Largest increment (if 1, then use fixed increment)
   externalWrap: Boolean = false,  // Additional wrapping condition supported?
-  inDelay: Int = 0                // Pipeline delay associated with inputs
+  inDelay: Int = 0,               // Pipeline delay associated with inputs
+  clkEn: Boolean = true,          // Whether additional enable is needed for slow clock support
+  change: Boolean = true          // Whether additional enable/change (i.e. update on previous counter max) is needed
 */
